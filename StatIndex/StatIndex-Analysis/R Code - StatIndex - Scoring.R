7d16c25ed17e05c9c1fc8c49013bfdb1ff530294
L. Bolton <liamthomasbolton@gmail.com>
2016-08-25 10:13:54 +0100

    Update R Code - StatIndex - Scoring.R

--- StatIndex/StatIndex-Analysis/R Code - StatIndex - Scoring.R
+++ StatIndex/StatIndex-Analysis/R Code - StatIndex - Scoring.R
@@ -4,11 +4,13 @@
 sum <- sum(Terms$frequency)
 print(sum)
 
+
 # Create quick if else statement to print statement if news content uses statistical insights or not
 if (sum < 1) {
 print("This piece of news content does not use statistical words or notation")
 } else 
 print("This piece of news content uses statistical words or notation")
+
 
 # Package loads at home, not at BBC. Similar issue when working with local server at BBC
 install.packages("qdap")
@@ -27,17 +29,21 @@ print(news_word_count)
 stat_percent <- (sum/news_word_count)*100
 print(stat_percent)
 
+
 # SCORE 3 - Mean statistical words based on dictionary dataset
 Stat_Score_3 <- mean(Terms$frequency)
 print(Stat_Score_3)
+
 
 # Order by frequency of statistical term
 order <- Terms[order(Terms$frequency, decreasing=TRUE),]
 print(order)
 
+
 # Merge the dataframe with statistical dictionary to determine complexity of statistical insights used
 df_merged <- merge(Terms, dictionary, by="content")
 df_merged <- df_merged[1:6]
+
 
 # Remove words in dictionary not used
 df_merged <- df_merged[!apply(df_merged, 1, function(x) {df_merged$frequency == 0}),]
@@ -49,6 +55,7 @@ library(ggplot2)
 ggplot(df_merged, aes(x=content, y=frequency)) + geom_bar(stat='identity', fill="#99e6ff") + coord_flip()
 theme(axis.title=element_blank()) + ggtitle(expression(atop("Frequency of Statistical Insights"))) + 
 theme(plot.title = element_text(size=20, hjust=0, color="black"))
+
 
 # SCORE 4 - Find mean 'complexity' based on a score that measures how advanced the statistical words are
 complexity <- mean(df_merged$score_3, na.rm=TRUE)
@@ -69,7 +76,13 @@ colnames(df_merged)[colnames(df_merged) == "frequency"] <- "Count"
 df_merged <- df_merged[, c("Term", "Count", "Complexity")]
 
 # Create the data table with statistical terms as the row
-a <- datatable(head(df_merged)) 
+a <- datatable(head(df_merged)) %>% 
+     formatStyle('Count', 
+     background = styleColorBar(df_merged$Count, 'blue'),
+     backgroundSize = '100% 90%',
+     backgroundRepeat = 'no-repeat',
+     backgroundPosition = 'center'
+   )
 a
 
 install.packages("htmlWidgets")
