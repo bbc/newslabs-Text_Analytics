cdfa29eb53494de971caed049ea4ebbe7e1ec394
L. Bolton <liamthomasbolton@gmail.com>
2016-08-26 14:16:09 +0100

    Update R Code - StatIndex - HTML.R

--- StatIndex/StatIndex-Analysis/R Code - StatIndex - HTML.R
+++ StatIndex/StatIndex-Analysis/R Code - StatIndex - HTML.R
@@ -15,6 +15,7 @@ news_text <- gsub('\\n', ' ', news_text)
 news_text <- paste(news_text, collapse = ' ')
 
 
+
 head(news_text)
 
 #### If looking to analyse your own news URL (e.g. this one from Quartz) - run the code through same as above
