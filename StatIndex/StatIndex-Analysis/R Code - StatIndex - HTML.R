809fc2a5dd5516e5a81b10c989bd3812b4485b6b
L. Bolton <liamthomasbolton@gmail.com>
2016-08-26 15:04:26 +0100

    Update R Code - StatIndex - HTML.R

--- StatIndex/StatIndex-Analysis/R Code - StatIndex - HTML.R
+++ StatIndex/StatIndex-Analysis/R Code - StatIndex - HTML.R
@@ -16,13 +16,13 @@ doc_html <- htmlTreeParse(news_URL_1, useInternal = TRUE)
 news_text <- unlist(xpathApply(doc_html, '//p', xmlValue))
 news_text <- gsub('\\n', ' ', news_text)
 news_text <- paste(news_text, collapse = ' ')
+head(news_text)
 
 # Extract numbers and bind to the text
 numbers <- gregexpr("[0-9]+", news_text)
 numbers_intext <- as.numeric(unique(unlist(regmatches(news_text, numbers))))
 rbind(news_text, numbers_intext)
 
-head(news_text)
 
 #### If looking to analyse your own news URL (e.g. this one from Quartz) - run the code through same as above
 doc_html <- htmlTreeParse("http://qz.com/762729/poor-data-is-hurting-african-countries-ability-to-make-good-policy-decisions/", useInternal = TRUE)
